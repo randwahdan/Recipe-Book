98f260c4ee68ff34975eaaced998a20dc39053c0
RandDev <80163738+randwahdan@users.noreply.github.com>
2024-12-10 23:20:38 +0300

    add recipe-service and shopping-list service

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,14 +2,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
   @Output() featureSelected = new EventEmitter<string>();
 
-  onSelect(feature: string){
+  onSelect(feature: string) {
     this.featureSelected.emit(feature);
-
-}
+  }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,26 +1,32 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild
+} from '@angular/core';
+
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.css'
+  styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef; 
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
-  constructor() {}
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
 
-  ngOnInit(): void {
-    
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit() {
   }
-  onAddItem(){
+
+  onAddItem() {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
-
-    const newIngredient = new Ingredient(ingName,ingAmount);
-    this.ingredientAdded.emit(newIngredient);
-
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.slService.addIngredient(newIngredient);
   }
+
 }
