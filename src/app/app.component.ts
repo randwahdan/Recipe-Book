a3ee9410d06be4f4e86d63489be7d59c6fb9d946
RandDev <80163738+randwahdan@users.noreply.github.com>
2024-12-16 16:18:40 +0300

    Improving the reactive services with observables(Subject)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadFeature ='recipe';
-
-  onNavigate(feature: string){
-    this.loadFeature = feature;
-
-  }
+ 
 }
